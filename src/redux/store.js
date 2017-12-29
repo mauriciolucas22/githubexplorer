08a9e9459f2f0d8f1bbe3cd252aac07f8e167015
Mauricio Lucas <mauricio.nq@hotmail.com>
2017-12-29 12:31:33 -0200

    redux-persist ok

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 import sagas from './sagas';
+import { AsyncStorage } from 'react-native';
 
 import reducers from './reducers';
+
+const rootReducer = persistCombineReducers({
+  key: 'root',
+  storage,
+}, reducers);
 
 const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
@@ -10,7 +18,12 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 const middleware = [sagaMiddleware];
 
 const createAppropriateStore = __DEV__ ? console.tron.createStore: createStore;
-const store = createAppropriateStore(reducers, applyMiddleware(...middleware));
+export const store = createAppropriateStore(
+  rootReducer,
+  applyMiddleware(...middleware),
+);
+
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(sagas);
 
